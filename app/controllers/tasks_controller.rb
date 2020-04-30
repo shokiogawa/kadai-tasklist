5236ab927755e5e05bea3f45245c5b61f3a79d16
shoki.ogawa <ec2-user@ip-172-31-87-214.ec2.internal>
2020-04-30 12:17:44 +0000

    resubmit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,60 @@
+class TasksController < ApplicationController
+  
+  def index
+      @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = "Taskは正常に投稿されました"
+      redirect_to @task
+    else 
+      flash.now[:danger] = "Taskが投稿されませんでした"
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+    
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.save
+      flash[:success] = "Taskは正常に投稿されました"
+      redirect_to @task
+      
+    else
+      flash.now[:danger] = "Taskが投稿されませんでした"
+      rendwe :new
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = "Messageは正常に削除されました"
+    redirect_to tasks_url
+  end
+end
+  
+  private
+  
+  def task_params
+   params.require(:task).permit(:content)
+  end
+
+
