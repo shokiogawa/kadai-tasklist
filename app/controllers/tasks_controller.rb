97397468a91e92b014364e40ee6a45468654b84f
shoki.ogawa <ec2-user@ip-172-31-87-214.ec2.internal>
2020-05-02 11:38:50 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
   
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
       @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -25,12 +26,10 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
     
   end
 
   def update
-    @task = Task.find(params[:id])
     
     if @task.update(task_params)
       flash[:success] = "Taskは正常に投稿されました"
@@ -43,7 +42,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = "Messageは正常に削除されました"
@@ -52,6 +50,10 @@ class TasksController < ApplicationController
 
   
   private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
   
   def task_params
    params.require(:task).permit(:content)
