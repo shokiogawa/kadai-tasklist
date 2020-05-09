3c9fb7323f536ab375d8f527ff0d43dd44badda2
shoki.ogawa <ec2-user@ip-172-31-87-214.ec2.internal>
2020-05-09 06:19:52 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
     root to: "tasks#index"
     resources :tasks
+    
+    get "signup", to: "users#index"
+    resources :users, only: [:index, :show, :new, :create]
     
     
 end
